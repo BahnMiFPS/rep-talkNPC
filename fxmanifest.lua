87d904514cdaba58f24f546eb2966849ce90e038
Quang Vu Luong <87252277+BahnMiFPS@users.noreply.github.com>
2023-09-23 14:10:41 +1000

    remove hotp

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,9 +5,9 @@ name "Rep Dev - Talk with NPC Ver 2"
 author "Q4D#1905 x BahnMiFPS#8508"
 version "1.0.0"
 
-client_scripts {'@hotp/build/client/token.lua', 'client/*.lua'}
+client_scripts { 'client/*.lua' }
 ui_page 'web/build/index.html'
-files {'web/build/index.html', 'web/build/**/*'}
+files { 'web/build/index.html', 'web/build/**/*' }
 
 shared_script 'config.lua'
 lua54 'yes'
